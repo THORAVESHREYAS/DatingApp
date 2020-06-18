d7e1b9366cf6921c00078816d6dcbeebd3ceca68
shreyas.thorave <shreyas.thorave@esyasoft.com>
2020-06-19 00:39:39 +0530

    End of section 4

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
             _context = dataContext;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
